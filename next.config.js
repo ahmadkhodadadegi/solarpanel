dba6b611509e748347a07db769fd1512e3c2e988
ahmad khodadadegi <ahmad.khodadadegi@gmail.com>
2024-03-01 23:32:34 +0330

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // @ts-check
- 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   "build": "next build && next export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
 }
- 
+
 module.exports = nextConfig
